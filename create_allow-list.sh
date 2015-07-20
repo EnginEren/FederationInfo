4f4a253dda71a5af20cd3e29dc929f930afaa7be
root <root@vocms037.cern.ch>
2015-07-20 11:48:44 +0200

    Few changes

--- create_allow-list.sh
+++ create_allow-list.sh
@@ -14,7 +14,5 @@ cat $BASE/tmp_total | cut -d : -f1 | sort -u > $FEDINFO/in/prod.txt
 cat $FEDINFO/in/prod.txt | cut -d : -f1 | awk -F. '{print "cms.allow host " "*."$(NF-1)"."$NF}' | sort -u > $FEDINFO/list_eu.allow
 rm $BASE/tmp_*
 
-#cd $FEDINFO
-#python parser.py
 
 exit 0;
