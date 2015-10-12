ca1fa8fcee1bca88484725dbcd095ceed039ac67
root <root@vocms037.cern.ch>
2015-10-12 16:30:06 +0200

    last_version

--- create_allow-list.sh
+++ create_allow-list.sh
@@ -5,27 +5,27 @@ BASE=/root
 FEDINFO=/root/FederationInfo
 export XRD_NETWORKSTACK=IPv4
 
-declare -a redirectors=("cms-xrd-global.cern.ch:1094" "cms-xrd-transit.cern.ch:1094")
+declare -a redirectors=("xrdcmsglobal01.cern.ch:1094" "xrdcmsglobal02.cern.ch:1094" "cms-xrd-transit.cern.ch:1094")
 
 for j in "${redirectors[@]}";do
-	if [ "$j" == "cms-xrd-global.cern.ch:1094" ]; then
-		xrdmapc --list all "$j" | grep Man  | awk '{print $3}' > $BASE/tmp_manlist
-		for i in $(cat $BASE/tmp_manlist);do
+	if [ "$j" == "xrdcmsglobal01.cern.ch:1094" ] || [ "$j" == "xrdcmsglobal02.cern.ch:1094" ]; then
+		xrdmapc --list all "$j" | grep Man  | awk '{print $3}' > $BASE/tmp_manlist_$j
+		for i in $(cat $BASE/tmp_manlist_$j);do
 		 	word=$(echo $i | cut -d ":" -f1 | awk -F"." '{print $NF}')	
 			if [ $word == "edu" ] || [ $word == "gov" ] || [ $word == "br" ] ;then
 				xrdmapc --list all $i > $BASE/tmp_us_$i	
-				cat $BASE/tmp_us_$i | awk '{if($2=="Man") print $3; else print $2}' | tail -n +2 >> $BASE/tmp_total_us
+				cat $BASE/tmp_us_$i | awk '{if($2=="Man") print $3; else print $2}' | tail -n +2 >> $BASE/tmp_total_us_$j
 			else
 				xrdmapc --list all $i > $BASE/tmp_$i	
-				cat $BASE/tmp_$i | awk '{if($2=="Man") print $3; else print $2}' | tail -n +2 >> $BASE/tmp_total_eu
+				cat $BASE/tmp_$i | awk '{if($2=="Man") print $3; else print $2}' | tail -n +2 >> $BASE/tmp_total_eu_$j
 			fi
 		done
 	
 
-		cat $BASE/tmp_total_eu | cut -d : -f1 | sort -u > $FEDINFO/in/prod.txt 
-		cat $BASE/tmp_total_us | cut -d : -f1 | sort -u >> $FEDINFO/in/prod.txt 
-		cat $BASE/tmp_total_eu | cut -d : -f1 | sort -u | awk -F. '{print "cms.allow host " "*."$(NF-1)"."$NF}' | sort -u > $FEDINFO/out/list_eu.allow
-		cat $BASE/tmp_total_us | cut -d : -f1 | sort -u | awk -F. '{print "cms.allow host " "*."$(NF-1)"."$NF}' | sort -u > $FEDINFO/out/list_us.allow
+		cat $BASE/tmp_total_eu_$j | cut -d : -f1 | sort -u > $FEDINFO/in/prod_$j.txt 
+		cat $BASE/tmp_total_us_$j | cut -d : -f1 | sort -u >> $FEDINFO/in/prod_$j.txt 
+		cat $BASE/tmp_total_eu_$j | cut -d : -f1 | sort -u | awk -F. '{print "cms.allow host " "*."$(NF-1)"."$NF}' | sort -u > $FEDINFO/out/list_eu_$j.allow
+		cat $BASE/tmp_total_us_$j | cut -d : -f1 | sort -u | awk -F. '{print "cms.allow host " "*."$(NF-1)"."$NF}' | sort -u > $FEDINFO/out/list_us_$j.allow
 
 	else
 		xrdmapc --list all "$j" | tail -n +2 | awk '{if($2=="Man") print $3; else print $2}' > $BASE/tmp_total
@@ -38,6 +38,13 @@ for j in "${redirectors[@]}";do
 done
 
 
+diff $FEDINFO/in/prod_xrdcmsglobal01.cern.ch\:1094.txt $FEDINFO/in/prod_xrdcmsglobal01.cern.ch\:1094.txt 
+stat=$(echo $?)
+if [ $stat == 1 ]; then
+	cat prod_xrdcmsglobal01.cern.ch:1094.txt prod_xrdcmsglobal02.cern.ch:1094.txt >> $FEDINFO/in/prod.txt	
+else
+	cp $FEDINFO/in/prod_xrdcmsglobal01.cern.ch\:1094.txt $FEDINFO/in/prod.txt
+fi	
 
 
 exit 0;
